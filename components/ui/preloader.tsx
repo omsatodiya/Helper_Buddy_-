edae24224bbf38046aff94155be7150784cfa041
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2025-02-07 01:47:18 +0530

    style: preloader bg

--- components/ui/preloader.tsx
+++ components/ui/preloader.tsx
@@ -14,7 +14,7 @@ const Preloader: React.FC<PreloaderProps> = ({ onLoadingComplete }) => {
       await progressControls.start({
         scaleX: 1,
         transition: {
-          duration: 1, // Reduced from 2 to 1 second
+          duration: 1,
           ease: "easeInOut",
         },
       });
@@ -27,28 +27,26 @@ const Preloader: React.FC<PreloaderProps> = ({ onLoadingComplete }) => {
 
   return (
     <motion.div
-      className="fixed inset-0 flex flex-col items-center justify-center bg-white"
+      className="fixed inset-0 flex flex-col items-center justify-center bg-black"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.2 }} // Reduced from 0.3 to 0.2 seconds
-    >
+      transition={{ duration: 0.2 }}>
       <motion.div
         className="mb-8"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-          duration: 0.3, // Reduced from 0.5 to 0.3 seconds
+          duration: 0.3,
           ease: "easeOut",
-        }}
-      >
+        }}>
         <div className="text-4xl font-bold text-blue-600">
           <img src="/images/logo.jpg" alt="" />
         </div>
       </motion.div>
 
-      <div className="relative w-64 h-1 bg-gray-200 rounded-full overflow-hidden">
+      <div className="relative w-64 h-1 bg-gray-800 rounded-full overflow-hidden">
         <motion.div
-          className="absolute top-0 left-0 h-full w-full bg-black origin-left"
+          className="absolute top-0 left-0 h-full w-full bg-white origin-left"
           initial={{ scaleX: 0 }}
           animate={progressControls}
         />
