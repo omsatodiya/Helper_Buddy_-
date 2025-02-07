eb75c0fd6e8a109b4efe9b1b7f4cfb44b283f3de
“Chetan <lost.alchemist69@gmail.com>
2025-02-07 15:13:49 +0530

    feat: Testimonials updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Testimonials from "@/components/ui/Testimonials"
+
 export default function Home() {
+
+
+
   return (
     <>
       <Header />
       <div className="h-screen"></div>  
-      <Testimonials />
+      <Testimonials  />
       <Footer />
     </>
   );
