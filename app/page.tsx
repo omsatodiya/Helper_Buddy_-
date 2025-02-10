77b4e3993b0bcac6e019d69ea9909886340d063a
jayp927 <jaypipaliya0101@gmail.com>
2025-02-10 16:47:24 +0530

    feat : faq added to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Testimonials from "@/components/ui/Testimonials"
+import FAQ from "@/components/faq/FAQ"
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <Header />
       <div className="h-screen"></div>  
+      <FAQ />
       <Testimonials  />
       <Footer />
     </>
