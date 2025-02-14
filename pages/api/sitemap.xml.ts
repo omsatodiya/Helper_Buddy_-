3a0b3c7c1153bf9edde9925e6a2fec2914683dd7
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2025-02-14 10:13:35 +0530

    fix: build error

--- pages/api/sitemap.xml.ts
+++ pages/api/sitemap.xml.ts
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const domain = 'https://dudhkela.netlify.app';
 
@@ -41,8 +40,8 @@ const pages = [
   }
 ];
 
-const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="sitemap.xsl"?>
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(page => `  <url>
     <loc>${domain}${page.url}</loc>
@@ -51,20 +50,14 @@ ${pages.map(page => `  <url>
     <priority>${page.priority}</priority>
   </url>`).join('\n')}
 </urlset>`;
+}
 
-// Define directories
-const publicDir = path.join(process.cwd(), 'public');
-const nextDir = path.join(process.cwd(), '.next/static');
-
-// Ensure directories exist
-[publicDir, nextDir].forEach(dir => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-});
-
-// Write sitemap to both directories
-fs.writeFileSync(path.join(publicDir, 'sitemap.xml'), sitemap);
-fs.writeFileSync(path.join(nextDir, 'sitemap.xml'), sitemap);
-
-console.log('Sitemap generated successfully in both public and .next/static directories!'); 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'application/xml; charset=UTF-8');
+  res.setHeader('Cache-Control', 'public, max-age=3600');
+  res.setHeader('X-Robots-Tag', 'all');
+  
+  const sitemap = generateSiteMap();
+  res.write(sitemap);
+  res.end();
+} 
