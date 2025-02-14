d42c060d41b194b58ae8e7fe00168cc387687794
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2025-02-14 09:40:12 +0530

    fix: search console appearance

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: 'og-image.jpg', // Add your OG image path
+        url: 'images/logo.jpg', // Add your OG image path
         width: 1200,
         height: 630,
         alt: 'Helper Buddy Services',
